7566b66547b778fdeb789da8b7bc5561a793bbb0
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2019-05-15 17:31:08 -0400

    init_proj.sh

--- scripts/init_proj.sh
+++ scripts/init_proj.sh
@@ -3,3 +3,13 @@
 project=`basename \`pwd\``
 
 python cc/gen.py project "${project}"
+
+cp config.js.tmpl config.js
+npm install
+ln -s ../config.js node_modules/config.js
+mkdir -p config
+mv node_modules/react-scripts/config/webpack.config.js config
+cd node_modules/react-scripts/config
+ln ../../../config/webpack.config.js ./
+cd ../../../
+npm start
